0d38ba3b0e2ef1f5aec8c420c3b3c2d641517b8f
Daniel Cross <crossy1686@gmail.com>
2019-05-19 20:58:13 +0200

    added useMemo hook

--- src/Input.js
+++ src/Input.js
@@ -1,4 +1,4 @@
-import React, { useRef, createContext } from 'react';
+import React, { useRef, createContext, useMemo } from 'react';
 import { useTitleInput } from './Hooks/useTitleInput';
 import Toggle from './Toggle';
 import Counter from './Counter';
@@ -8,10 +8,19 @@ export const UserContext = createContext();
 const Input = () => {
 	const [ name, setName ] = useTitleInput('');
 	const ref = useRef();
+
+	const reverseWord = (word) => {
+		return word.split('').reverse().join('');
+	};
+
+	const title = 'Welcome to a React Hooks experiment!';
+
+	const titleReversed = useMemo(() => reverseWord(title), [ title ]);
+
 	return (
 		<UserContext.Provider value={{ user: true }}>
 			<div className="wrapper" ref={ref}>
-				<h1 onClick={() => ref.current.classList.add('new-class-name')}>HOOK INPUT</h1>
+				<h1 onClick={() => ref.current.classList.add('new-class-name')}>{titleReversed}</h1>
 				<form
 					onSubmit={(e) => {
 						e.preventDefault();
@@ -20,7 +29,7 @@ const Input = () => {
 					<button>Submit</button>
 				</form>
 				<Toggle />
-				<Counter />
+				{/* <Counter /> */}
 			</div>
 		</UserContext.Provider>
 	);
